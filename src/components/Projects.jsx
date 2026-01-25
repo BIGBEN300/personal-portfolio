adecb442b4cf743bdda2454d49f455c0e2ef521d
BIGBEN300 <benrobbins@outlook.com>
2026-01-25 16:49:05 +0000

    Updates
    Added and Changed card image

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -46,7 +46,7 @@ export const Projects = () => {
                 >
                     <motion.div 
                         className="project-image" 
-                        style={{ backgroundImage: "url('/projects/ph.png')" }}
+                        style={{ backgroundImage: "url('/projects/QuickQR.png')" }}
                         whileHover={{ scale: 1.05, transition: { duration: 0.2 } }}
                     />
                         <h3> Quick QR</h3>
