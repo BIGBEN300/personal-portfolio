7be0ae5d61a5b430329eabafe8c165846ab13486
BIGBEN300 <benrobbins@outlook.com>
2026-02-02 15:00:21 +0000

    Updates to project section and a bug fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,7 +44,7 @@ export const Navbar = () => {
                     <a href="#projects"> Projects</a>
                 </motion.li>
                 <motion.li variants={fadeInUp} whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.95 }}>
-                    <a href="#contant"> Contact</a>
+                    <a href="#contact"> Contact</a>
                 </motion.li>
             </motion.ul>
         </motion.nav>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -51,15 +51,16 @@ export const Projects = () => {
                     />
                         <h3> Quick QR</h3>
                         <p>
-                            A modern QR Code Generator built with JavaScript, 
-                            featuring a user friendly design to that has a step wizard to design and create QR codes in real-time
+                            project-quickqr is an open-source backend service for generating customizable QR codes in web applications. Built with Node.js and Express, 
+                            it provides a dynamic API that creates QR code images on demand based on user input, with support for size and color customization. 
+                            The project emphasizes performance, scalability, and ease of integration, making it suitable for use in modern web architectures and extensible for a wide range of use cases.
                         </p>
                         <div className="project-tech">
                             <span>JavaScript</span>
                             <span>HTML & CSS</span>
                         </div>
                         <div className="project-url">
-                            <a href="https://github.com/BIGBEN300" target="blank">Visit Website</a>
+                            <a href="https://github.com/BIGBEN300/project-quickqr" target="blank">Visit GitHub Repo</a>
                         </div>
                 </motion.div>
 
@@ -70,23 +71,48 @@ export const Projects = () => {
                 >
                     <motion.div 
                         className="project-image" 
-                        style={{ backgroundImage: "url('/projects/ph.png')" }}
+                        style={{ backgroundImage: "url('/projects/projectUE.png')" }}
                         whileHover={{ scale: 1.05, transition: { duration: 0.2 } }}
                     />
-                        <h3> Productivity Timer</h3>
+                        <h3> Project UE</h3>
                         <p>
-                            A sleek Productivity Timer application with customizable work sessions, statistics tracking and dark mode support
+                            With my team we created a game in Unreal Engine. My role within the group was Programmer and Game Engine Level Designer. 
+                            In this role, I was tasked to make the user interfaces in the game (main menu, pause menu, settings etc.), different puzzle systems, equipment system and the games NPC’s (enemies and animals).
+                            Alongside my taskes I also got opportunities to try different aspects on game making like 3D modelling and concept art, which is shown in the game design document. 
                         </p>
                         <div className="project-tech">
-                            <span>React JS</span>
-                            <span>TypeScript</span>
-                            <span>TailwindCSS</span>
+                            <span>C++</span>
+                            <span>Blueprints</span>
+                            <span>Blender</span>
                         </div>
                         <div className="project-url">
                             <a href="https://github.com/BIGBEN300" target="blank">Visit Website</a>
                         </div>
                 </motion.div>
                 
+                <motion.div 
+                    className="project-card" 
+                    variants={fadeInUp} 
+                    whileHover={{ y: -10, transition: { duration: 0.2 } }}
+                >
+                    <motion.div 
+                        className="project-image" 
+                        style={{ backgroundImage: "url('/projects/chatbot.png')" }}
+                        whileHover={{ scale: 1.05, transition: { duration: 0.2 } }}
+                    />
+                        <h3> Personal Chatbot</h3>
+                        <p>
+                            My current project I'm working on is a personal Chatbot that I will be able to answer questions and explain them in detail, help with learning and skills building,
+                            and decision making/brainstorming.
+                        </p>
+                        <div className="project-tech">
+                            <span>Javascript</span>
+                            <span>HTML & CSS</span>
+                        </div>
+                        <div className="project-url">
+                            <a href="https://github.com/BIGBEN300" target="blank">Visit Website</a>
+                        </div>
+                </motion.div>
             </motion.div>
     </motion.section>
     );
